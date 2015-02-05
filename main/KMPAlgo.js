a76cf139d90c1a2555dfd926bdb71e16440f66bf
ago <omarebo@gmail.com>
2015-02-05 00:34:03 +0000

    to fix: the same KMPAlgo object is loaded every time

--- main/KMPAlgo.js
+++ main/KMPAlgo.js
@@ -6,6 +6,8 @@ module.exports = function(needle, haystack) {
   this._needle = needle
   this._haystack = haystack
 
+  this._currentMatchStart = 0
+
   this._kmpTable = function () {
     var nextCandidateIndex = 0
     var kmpTableInTheMake = [0, 0]
@@ -18,16 +20,18 @@ module.exports = function(needle, haystack) {
   }()
 
   return function () {
-    m = 0 // current match start
+    m = _currentMatchStart // current match start
     i = 0 // current match length
 
     while (m + i < _haystack.length)
       if (_needle[i] == _haystack[m + i]) {
         ++i;
-        if (i == _needle.length) return m + 1
+        if (i == _needle.length) return _currentMatchStart = m + 1
       } else
         if (i == 0) ++m
         else m = m + i - (i = _kmpTable[i])
+
+    _currentMatchStart = m + 1
 
     return -1
   }
--- test/KMPAlgoTest.js
+++ test/KMPAlgoTest.js
@@ -6,7 +6,25 @@ var assert = require("assert")
 
 var matcher = require("../main/KMPAlgo")("b", "abc")
 
-it('simplest case', function(){
+it('simplest case one match', function(){
   assert.equal(2, matcher());
+  assert.equal(-1, matcher());
+})
+
+it('"extreme" case', function(){
+  var matcher = require("../main/KMPAlgo")("aa", "aaaaababaabaa")
+  assert.equal(1, matcher());
+  assert.equal(2, matcher());
+  assert.equal(3, matcher());
+  assert.equal(4, matcher());
+  assert.equal(9, matcher());
+  assert.equal(12, matcher());
+})
+
+it('2 searches mixing: ERROR!', function(){
+  var matcher1 = require("../main/KMPAlgo")("hi", "xxhihixxxhix")
+  assert.equal(3, matcher1());
+  var matcher2 = require("../main/KMPAlgo")("hello", "xxxhelloxxhellox")
+  assert.equal(4, matcher1()); // it should be 5!
 })
 
